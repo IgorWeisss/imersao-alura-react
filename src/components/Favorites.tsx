09242c7bfbdf05441ee35a376c0a371766522892
Igor - Ubuntu <igorweiss@hotmail.com.br>
2022-11-09 15:04:29 -0300

    Aula 1 Completa

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -14,11 +14,11 @@ export function Favorites ({ favorites }:FavoritesProps) {
   return (
       <div className='px-4 py-4 md:py-8'>
         <h3 className='font-bold text-base mb-4'>Favoritos</h3>
-        <ul className='flex gap-2'>
+        <ul className='grid grid-cols-3 sm:grid-cols-4 justify-center md:flex md:flex-wrap md:justify-start self-center gap-2'>
           {favorites.map((fav) => {
             return (
-              <li className='w-14 md:w-[5rem] flex text-center justify-items-stretch'>
-                <a href={fav.url}>
+              <li key={fav.name} className='flex text-center justify-center'>
+                <a href={fav.url} className="flex h-full flex-col items-center w-28 gap-2">
                   <img
                     src={fav.avatar}
                     alt={`Canal ${fav.name}`}
